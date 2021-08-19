1a0e82a88df6d309b1b78d54168e90be81dac72d
Victoria Acuna <victoria.ines.11@gmail.com>
2021-08-18 22:39:51 -0400

    add specifications/messages/exceptionmanager to routes

--- routes/company.js
+++ routes/company.js
@@ -4,9 +4,21 @@ let Company = require('../models/company.model');
 
 // get all the companies
 router.route('/').get((req, res) => {
+
     Company.find()
-    .then(companies => res.json(companies))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .then(companies => {
+      res.status.apply(200).json({
+        success: true,
+        data: companies
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        message: 'Server error: ' + err
+      })
+    });
+
 });
 
 // add a new company
@@ -14,32 +26,44 @@ router.route('/add').post((req, res) => {
 
   // Check if email already exist.
   Company.findOne({email:req.body.email}, (err, item) => {
+
     if(err){
+
       res.status(500).json({
         success: false,
-        error: 'Server Error ' + err
+        message: 'Server error: ' + err
       });
+
     } else {
+
       if(Boolean(item)){
+
         res.status(400).json({
           success: false,
-          error: 'Ese email ya existe'
+          message: 'This email already exists.'
         });
+
       } else {
 
         // Check if company name already exist
         Company.findOne({full_name: req.body.full_name}, (err2, item2) => {
+
           if(err2){
+
             res.status(500).json({
               success: false,
-              error: 'Server Error ' + err2
+              message: 'Server error: ' + err2
             });
+
           } else {
+
             if(Boolean(item2)){
+
               res.status(400).json({
                 success: false,
-                error: 'Ese full name ya existe'
+                message: 'This company is already registered'
               });
+        
             } else {
 
               const {
@@ -60,11 +84,16 @@ router.route('/add').post((req, res) => {
               });
 
               newCompany.save()
-                .then((data) => res.json(data))
+                .then((data) => {
+                  res.status(200).json({
+                    success: true,
+                    data: data
+                  })
+                })
                 .catch(err => {
                   res.status(500).json({
                     success: false,
-                    error: 'Server Error ' + err
+                    message: 'Server error: ' + err
                   });
                 });
             }
@@ -84,14 +113,32 @@ router.route('/add').post((req, res) => {
 router.route('/:id').get((req, res) => {
 
   Company.findById(req.params.id)
-  .then(company => {
-    if(Boolean(company)){
-      res.json(company)
+  .then(item => {
+
+    if(Boolean(item)){
+
+      res.status(200).json({
+        success: true,
+        data: item
+      })
+
     } else{
-      res.status(404).json('Company not found')
+
+      res.status(404).json({
+        success: false,
+        message: 'This company is not registered'
+      })
+
     }
   })
-  .catch(err => res.status(500).json('Server error: ' + err));
+  .catch(err => {
+    
+    res.status(500).json({
+      success: false,
+      message: 'Server error: ' + err
+    })
+  
+  });
   
 });
 
@@ -100,23 +147,27 @@ router.route('/:id').delete((req, res) => {
 
   Company.deleteOne({_id: req.params.id}, (err, item) => {
     if(err){
+
       res.status(500).json({
         success: false,
-        message: 'Server Error ' + err
+        message: 'Server error: ' + err
       });
+
     } else {
 
+      // Check if we found the id and deleted the item
       if(item.deletedCount==1){
 
-        return res.json({
+        return res.status(200).json({
           success: true,
           message: 'Company deleted'
         });
 
       } else {
+
         return res.status(404).json({
           success: false,
-          message: 'Company not found'
+          message: 'This company is not registered'
         });
       }
       
@@ -125,20 +176,50 @@ router.route('/:id').delete((req, res) => {
 
 });
 
+
 router.route('/update/:id').post((req, res) => {
-    Company.findById(req.params.id)
-      .then(company => {
-        company.email = req.body.email;
-        company.password = req.body.password;
-        company.full_name = req.body.full_name;
-        company.active = req.body.active;
-        company.accepted = req.body.accepted;
-        company.mainImg = req.body.mainImg;
-  
-        company.save()
-          .then(() => res.json('Company updated!'))
-          .catch(err => res.status(400).json('Error: ' + err));
-      })
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
+
+  Company.findById(req.params.id)
+    .then((item) => {
+
+      if(Boolean(item)){
+
+        const {
+          email, 
+          full_name,
+          mainImg
+        } = req.body
+      
+        // when we add a company, it will be activate and waiting for acceptance
+        const newCompany = new Company({
+          email, 
+          full_name,
+          active: item.active,
+          accepted: item.accepted,
+          password: item.password,
+          mainImg
+        });
+
+        newCompany.save()
+        .then(() => res.json('Company updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+
+      } else {
+
+        res.status(404).json({
+          success: false,
+          message: 'This company is not registered'
+        });
+        
+      }
+
+      
+    })
+    .catch(err => res.status(500).json({
+      success: false,
+      message: 'Server error: ' + err
+    }));
+});
+
+
 module.exports = router;
